05b6b91610998a2000c49b53ff0b831e9277dcc1
​ex_zhoushaowei <123456>
2018-02-05 17:29:04 +0800

    third update

--- src/main/java/com/onebank/Windows2LinuxSubmitJob2/Windows2LinuxSubmitJob2Driver.java
+++ src/main/java/com/onebank/Windows2LinuxSubmitJob2/Windows2LinuxSubmitJob2Driver.java
@@ -12,19 +12,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Windows2LinuxSubmitJob2Driver {
 
 	public static void main(String[] args) throws Exception {
-		if(args.length != 3){
+	/*	if(args.length != 3){
 			System.err.println("input args counts error!!");
 			System.err.println("please input jobName,inputpath,outputpath,date!!");
 		    System.exit(0);
-		}
-		System.out.println(args[0]);
+		}*/
+		//System.out.println(args[0]);
 		Configuration conf = new Configuration();
+		
+//		conf.setBoolean("mapreduce.app-submission.cross-platform", true);// 配置使用跨平台提交任务
+//		
+//		conf.set("fs.defaultFS", "hdfs://192.168.230.102:9000/test000"); // 指定namenode
+//		
+//		conf.set("mapreduce.framework.name", "yarn"); // 指定使用yarn框架
+//		
+//		conf.set("yarn.resourcemanager.address", "192.168.230.101:8032"); // 指定ResourceManager
+//		
+//		conf.set("yarn.resourcemanager.scheduler.address", "192.168.230.101:8030");// 指定资源分配器  
+		
+		
+		
 		//conf.set("date", args[3]);
-		Job job = Job.getInstance(conf, args[0]);
+		Job job = Job.getInstance(conf, "test2");
 		
 		/*System.out.println(args[1]);
 		System.out.println(conf.get("date"));*/
 		job.setJarByClass(Windows2LinuxSubmitJob2Driver.class);
+		
+		job.setJar("F:\\eeeee\\Windows2LinuxSubmitJob2.jar");
 		
 		job.setMapOutputKeyClass(Text.class);	
 		job.setMapOutputValueClass(IntWritable.class);
@@ -35,10 +50,10 @@ public class Windows2LinuxSubmitJob2Driver {
 		job.setMapperClass(Windows2LinuxSubmitJob2Map.class);
 		job.setReducerClass(Windows2LinuxSubmitJob2Reduce.class);
 		
-		Path intputPath = new Path(args[1]);
+		Path intputPath = new Path("hdfs://192.168.230.102:9000/test000/input/word.txt");
 		FileInputFormat.addInputPath(job, intputPath);	
 		
-		Path outputPath = new Path(args[2]);
+		Path outputPath = new Path("hdfs://192.168.230.102:9000/test000/output");
 		FileOutputFormat.setOutputPath(job, outputPath);	
 		
 		boolean waitForCompletion = job.waitForCompletion(true);
